9a012f17bff6f1ac2ee2afa6fc49260f33babe50
culverstedman <culver.stedman@kellogg.northwestern.edu>
2026-03-17 20:58:43 +0000

    updated new code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/login"
   end
 end
